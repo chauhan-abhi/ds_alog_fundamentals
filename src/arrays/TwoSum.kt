1d03d4545c7741f11fba0a37242159cf4a8c3206
chauhan-abhi <chauhanabhi1245@gmail.com>
2021-12-24 15:17:54 +0530

    Implemented  MergeSortedArrays

--- src/arrays/TwoSum.kt
+++ src/arrays/TwoSum.kt
@@ -4,19 +4,34 @@ import Problem
 
 class TwoSum: Problem {
 
-    fun twoSum(list: List<Int>, sum: Int): List<Int> {
+    fun twoSum(list: List<Int>, sum: Int) : IntArray {
         var diff: Int
         val map = HashSet<Int>()
         for(element in list) {
             diff = sum - element
-            if(map.contains(diff)) return listOf(element, diff)
+            if(map.contains(diff)) return intArrayOf(element, diff)
             else map.add(element)
         }
-        return listOf()
+        return intArrayOf()
+    }
+
+    fun twoSumIndex(nums: IntArray, target: Int): IntArray {
+        var diff: Int
+        val map = HashMap<Int, Int>()
+        for(i in nums.indices) {
+            val element = nums[i]
+            diff = target - element
+            if(map.containsKey(diff)) return intArrayOf(i, map.getOrDefault(diff, 0))
+            else map[element] = i
+        }
+        return intArrayOf()
     }
 
     override fun solve() {
         val array = listOf(5, 1, 22, 25, 6, -1, 8, 10)
-        print(twoSum(array, 6))
+        val array2 = intArrayOf(5, 1, 22, 25, 6, -1, 8, 10)
+        println(twoSum(array, 6))
+        val output = twoSumIndex(array2, 6)
+        for (x in output) print("$x  ")
     }
 }
