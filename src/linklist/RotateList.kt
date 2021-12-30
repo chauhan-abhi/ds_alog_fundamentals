6ad9c69a475b35a38f93aec76c7e51882c5324dd
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2021-12-30 20:38:03 +0530

    Implemented  MergeSortedList.kt, RemoveElementsNode.kt, RotateList.kt

---
+++ src/linklist/RotateList.kt
@@ -0,0 +1,38 @@
+package linklist
+
+import Problem
+
+
+class RotateList: Problem {
+
+    // 1 -> 4 -> 5 -> 8
+
+    private fun rotateList(head: Node?): Node? {
+        val newHead: Node?
+        var prev: Node? = null
+        var curr = head
+        var next: Node?
+        while (curr != null) {
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        newHead = prev
+        return newHead
+    }
+
+
+    override fun solve() {
+        val root = Node(1)
+        root.next = Node(4)
+        root.next?.next = Node(5)
+        root.next?.next?.next= Node(8)
+
+        var modifiedRoot = rotateList(root)
+        while (modifiedRoot != null) {
+            print("${modifiedRoot.value} ")
+            modifiedRoot = modifiedRoot.next
+        }
+    }
+}
