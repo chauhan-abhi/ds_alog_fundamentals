a94868c916f04f8a90c44f9227c2345716fbb326
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2021-12-30 14:47:52 +0530

    Implemented  DetectCycle.kt

---
+++ src/linklist/DetectCycle.kt
@@ -0,0 +1,28 @@
+package linklist
+
+import Problem
+
+class DetectCycle: Problem {
+
+    private fun hasCycle(head: Node?): Boolean {
+        if (head == null) return false
+        var slow = head
+        var fast = head
+        while (slow != null && fast?.next != null) {
+            slow = slow.next
+            fast = fast.next?.next
+            if (slow == fast) return true
+        }
+        return false
+
+    }
+
+    override fun solve() {
+        val root = Node(1)
+        root.next = Node(2)
+        root.next?.next = Node(3)
+        root.next?.next?.next= Node(4)
+        root.next?.next?.next?.next= root.next
+        println(hasCycle(root))
+    }
+}
