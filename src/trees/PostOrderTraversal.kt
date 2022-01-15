3f77268386eec5b5e7823f0ff0e5c0784ae783b8
chauhan-abhi <chauhanabhi1245@gmail.com>
2022-01-15 19:24:24 +0530

    Implemented  Tree problems

---
+++ src/trees/PostOrderTraversal.kt
@@ -0,0 +1,59 @@
+package trees
+
+import Problem
+import java.util.*
+
+
+class PostOrderTraversal: BaseTreeProblem(), Problem {
+
+    private fun postorderTraversal(root: TreeNode?): List<Int> {
+        val list = LinkedList<Int>()
+        postHelper(list, root)
+        return list
+    }
+
+    private fun postorderIterative(root: TreeNode?): List<Int> {
+        val postOrder: MutableList<Int> = LinkedList()
+        val visitingStack = Stack<TreeNode>()
+        if (root == null) return postOrder
+        var cur = root
+
+        while (cur != null || !visitingStack.empty()) {
+            while (!isLeaf(cur)) {
+                visitingStack.push(cur)
+                cur = cur?.left
+            }
+            if (cur != null) postOrder.add(cur.value)
+            while (!visitingStack.empty() && cur == visitingStack.peek().right) {
+                cur = visitingStack.pop()
+                postOrder.add(cur.value)
+            }
+            cur = if (visitingStack.empty()) null else visitingStack.peek().right
+        }
+
+        return postOrder
+    }
+
+    private fun isLeaf(r: TreeNode?): Boolean {
+        return if (r == null) true else r.left == null && r.right == null
+    }
+
+    private fun postHelper(list: LinkedList<Int>, root: TreeNode?) {
+        if (root == null) return
+        postHelper(list, root.left)
+        postHelper(list, root.right)
+        list.add(root.value)
+
+    }
+
+    override fun getTree(): TreeNode = TreeUtils.getTestTree()
+
+    override fun solve() {
+        val res = postorderTraversal(getTree())
+        val resIt = postorderIterative(getTree())
+        println(res.toString())
+        println(resIt.toString())
+
+    }
+
+}
---
+++ src/trees/PreOrderTraversal.kt
@@ -0,0 +1,47 @@
+package trees
+
+import Problem
+import java.util.*
+
+class PreOrderTraversal: BaseTreeProblem(), Problem {
+
+    private fun preorderTraversal(root: TreeNode?): List<Int> {
+        val list = LinkedList<Int>()
+
+        preHelper(list, root)
+        return list
+    }
+
+    private fun preorderIterative(root: TreeNode?): List<Int> {
+        val pre: MutableList<Int> = LinkedList()
+        if (root == null) return pre
+        val visitingStack = Stack<TreeNode>()
+        visitingStack.push(root)
+        while (!visitingStack.empty()) {
+            val visiting = visitingStack.pop()
+            pre.add(visiting.value)
+            if (visiting.right != null) visitingStack.push(visiting.right)
+            if (visiting.left != null) visitingStack.push(visiting.left)
+        }
+        return pre
+    }
+
+    private fun preHelper(list: LinkedList<Int>, root: TreeNode?) {
+        if (root == null) return
+
+        list.add(root.value)
+        preHelper(list, root.left)
+        preHelper(list, root.right)
+    }
+
+    override fun getTree(): TreeNode = TreeUtils.getTestTree()
+
+    override fun solve() {
+        val res = preorderTraversal(getTree())
+        val resIt = preorderIterative(getTree())
+        println(res.toString())
+        println(resIt.toString())
+
+    }
+
+}
