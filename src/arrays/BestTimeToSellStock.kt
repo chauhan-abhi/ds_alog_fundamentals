c39075bcd5931ddb69cbfd58c1a47211de9a16ba
chauhan-abhi <chauhanabhi1245@gmail.com>
2022-01-22 17:37:37 +0530

    Best to sell stock II

--- src/arrays/BestTimeToSellStock.kt
+++ src/arrays/BestTimeToSellStock.kt
@@ -5,7 +5,7 @@ import kotlin.math.max
 
 class BestTimeToSellStock: Problem {
 
-    private fun maxProfit(prices: IntArray): Int {
+    private fun bestTimeToSellStock(prices: IntArray): Int {
         if (prices.size <= 1) return 0
         var max = 0
         var minSoFar  = prices[0]
@@ -17,10 +17,22 @@ class BestTimeToSellStock: Problem {
         return max
     }
 
+    private fun bestTimeToSellStockII(prices: IntArray): Int {
+        if (prices.size <= 1) return 0
+        var max = 0
+        for (idx in 1 until prices.size) {
+            if (prices[idx] > prices[idx-1])
+                max+= prices[idx]-prices[idx-1]
+        }
+        return max
+    }
+
     override fun solve() {
         val array = intArrayOf(7,1,5,3,6,4)
         val array2 = intArrayOf(3,2,6,5,0,3)
-        print(maxProfit(array))
-        print(maxProfit(array2))
+        val array3 = intArrayOf(7,1,5,3,6,4)
+        print(bestTimeToSellStock(array))
+        print(bestTimeToSellStock(array2))
+        print(bestTimeToSellStockII(array3))
     }
 }
