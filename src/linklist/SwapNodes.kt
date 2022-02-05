fccb60e03b63e0e0b2c600f3146ebc3b5243baaf
chauhan-abhi <chauhanabhi1245@gmail.com>
2022-02-05 12:31:46 +0530

    SwapNodes.kt implemented

---
+++ src/linklist/SwapNodes.kt
@@ -0,0 +1,42 @@
+package linklist
+
+import Problem
+
+
+class SwapNodes: Problem {
+
+    // 1 -> 4 -> 5 -> 8
+    // https://leetcode.com/problems/swap-nodes-in-pairs/
+
+    private fun swapPairs(head: Node?): Node? {
+        val dummy = Node(-1)
+        dummy.next = head
+        var prev: Node? = dummy
+        var curr : Node?= dummy
+
+        while (curr?.next != null && curr.next?.next != null) {
+            val first: Node? = curr.next
+            val second: Node? = curr.next?.next
+            first?.next = second?.next
+            curr.next = second
+            curr.next?.next = first
+            curr = curr.next?.next
+        }
+
+        return dummy.next
+    }
+
+
+    override fun solve() {
+        val root = Node(1)
+        root.next = Node(4)
+        root.next?.next = Node(5)
+        root.next?.next?.next= Node(8)
+
+        var modifiedRoot = swapPairs(root)
+        while (modifiedRoot != null) {
+            print("${modifiedRoot.value} ")
+            modifiedRoot = modifiedRoot.next
+        }
+    }
+}
