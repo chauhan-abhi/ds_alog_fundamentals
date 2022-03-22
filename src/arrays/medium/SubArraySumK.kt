e7be05baed2920c3f13ab60ce52e1a2989864181
chauhan-abhi <chauhanabhi1245@gmail.com>
2022-03-22 21:18:01 +0530

    EqualSubsetSumPartition.kt and SubSet Sum implemented

--- src/arrays/medium/SubArraySumK.kt
+++ src/arrays/medium/SubArraySumK.kt
@@ -4,10 +4,13 @@ import Problem
 
 /*
 * https://leetcode.com/problems/subarray-sum-equals-k/
+* if the cumulative sum up to two indices,
+* say ii and jj is at a difference of kk i.e. if sum[i] - sum[j] = ksum[i]−sum[j]=k,
+* the sum of elements lying between indices ii and jj is kk.
 * */
 class SubArraySumK: Problem {
 
-    private  fun subarraySum(nums: IntArray, k: Int): Int {
+    private fun subArraySum(nums: IntArray, k: Int): Int {
 
         val map = hashMapOf<Int, Int>()
         var count = 0
@@ -26,8 +29,8 @@ class SubArraySumK: Problem {
         val input = intArrayOf(1,2,3)
         val input2 = intArrayOf(1,1,1)
         val input3 = intArrayOf(3,4,7,2,-3,1,4,2)
-        println(subarraySum(input, 3))
-        println(subarraySum(input2, 2))
-        println(subarraySum(input3, 7))
+        println(subArraySum(input, 3))
+        println(subArraySum(input2, 2))
+        println(subArraySum(input3, 7))
     }
 }
