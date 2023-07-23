0e2c8a8f04825a1bcbc22747813ce1d0b844e022
abhchauhan <abhchauhan@expediagroup.com>
2023-07-23 18:14:31 +0530

    RemoveDuplicatesInPlace.kt atmost K occurrences done

--- src/arrays/RemoveDuplicatesInPlace.kt
+++ src/arrays/RemoveDuplicatesInPlace.kt
@@ -5,6 +5,7 @@ import Problem
 
 class RemoveDuplicatesInPlace: BaseProblem(), Problem {
 
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array
     private fun removeDuplicates(nums: IntArray): Int {
         var start = 0
         val end = nums.size-1
@@ -17,13 +18,39 @@ class RemoveDuplicatesInPlace: BaseProblem(), Problem {
             i++
         }
         return start+1
-
     }
+
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
+    private fun removeDuplicatesAtMostKOccurrences(nums: IntArray): Int {
+        val k = 2
+        if (nums.isEmpty()) return 0
+
+        var start = 1
+        val end  = nums.size
+        var count = 1
+
+        for (i in 1 until end) {
+            if (nums[i] == nums[i-1]) {
+                if (count < k) {
+                    nums[start++] = nums[i]
+                }
+                count++
+            } else {
+                count = 1
+                nums[start++] = nums[i]
+            }
+        }
+        nums.forEach { println(it) }
+        return start
+    }
+
+
 
     override fun solve() {
         val array1 = intArrayOf(0,0,1,1,1,2,2,3,3,4)
         val array2 = intArrayOf(0,1,2,3,4,5)
-        println(removeDuplicates(array1))
-        println(removeDuplicates(array2))
+        //println(removeDuplicates(array1))
+        //println(removeDuplicates(array2))
+        println(removeDuplicatesAtMostKOccurrences(array1))
     }
 }
